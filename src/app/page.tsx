d5af13e08016669c819eccd14fa012c6d6e45283
Ken-Anazodo <kenanazodo43@gmail.com>
2025-07-24 23:16:54 +0100

    landing page contd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,22 +13,39 @@ export default function Home() {
           <div className='w-full h-screen relative bg-black'> 
             <div className='absolute top-0 left-0 h-full w-full flex'>
               <div className='h-full w-[85% pl-8'>
-                <div className='flex align-middle mt-20'>
-                  <div className='w-20 mr-10'>
+                
+                <div className='flex align-middle mt-28 p-4 statement'>
+                  <div className='w-25 mr-10'>
                     <p>Dive into an Experience</p>
                   </div>
-                  <div className='w-20'>
+                  <div className='w-25'>
                     <p>Cruise with Leisure</p>
                   </div>
                 </div>
 
-                <div className='mt-20 w-60'>
-                  <p>Find The Hottest Night Life Events, Vibes And Meet New People</p>
+                <div className='mt-12 w-[50vw] p-4 flex justify-between'>
+                  <p className='hero-summary w-[70%]'>Find The Hottest Night Life Events, Vibes And Meet New People.</p>
+                  <p className='num'>(01)</p>
                 </div>
 
-                <div className='mt-11'>
-                  <p className='night'>NIGHT LIFE</p>
+                <div className='mt-5 flex align-middle gap-3'>
+                  <p className='night'>NIGHT</p>
+                  <div className='life px-5 h-full text-zinc-900 mt-[3.3vw]'>
+                    <p className='m-0 p-0'>L</p>
+                    <p className='m-0 p-0'>I</p>
+                    <p className='m-0 p-0'>F</p>
+                    <p className='m-0 p-0'>E</p>
+                  </div>
                 </div>
+
+                {/* <div className='flex align-middle justify-center'>
+                  <div className='w-25 mr-10'>
+                    <p>Dive into an Experience</p>
+                  </div>
+                  <div className='w-25'>
+                    <p>Cruise with Leisure</p>
+                  </div>
+                </div> */}
               </div>
               {/* <div className='h-full w-[15% bg-stone-100'></div> */}
             </div> 
